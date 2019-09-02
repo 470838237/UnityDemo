3ece37be13e1f60166fb0547a343b4e13f5cef65
heyuan <470838237@qq.com>
2019-09-02 17:42:02 +0800

    unitydemo

---
+++ UnityDemo/Assets/Scriptes/IosSDKImpl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace honorsdk
+{
+    class IosSDKImpl : HonorSDK
+    {
+ 
+
+        public IosSDKImpl() {
+
+
+        }
+       
+       
+    }
+}

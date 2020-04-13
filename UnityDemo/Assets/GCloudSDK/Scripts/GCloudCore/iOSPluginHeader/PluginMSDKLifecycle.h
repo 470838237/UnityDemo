dc1ad37000317644f3a0060db9e41b6b99939143
heyuan <470838237@qq.com>
2020-04-13 09:41:20 +0800

    adsd

--- UnityDemo/Assets/GCloudSDK/Scripts/GCloudCore/iOSPluginHeader/PluginMSDKLifecycle.h
+++
@@ -1,8 +0,0 @@
-#ifndef PluginMSDKLifecycle_h
-#define PluginMSDKLifecycle_h
-
-#import <GCloudCore/GCloudAppLifecycleObserver.h>
-@interface PluginMSDKLifecycle : NSObject <GCloudAppLifecycleObserver>
-
-@end
-#endif /* PluginMSDKLifecycle_h */
--- UnityDemo/Assets/GCloudSDK/Scripts/GCloudCore/iOSPluginHeader/PluginReportLifecycle.h
+++
@@ -1,8 +0,0 @@
-#ifndef PluginReportLifecycle_h
-#define PluginReportLifecycle_h
-
-#import <GCloudCore/GCloudAppLifecycleObserver.h>
-@interface PluginReportLifecycle : NSObject <GCloudAppLifecycleObserver>
-
-@end
-#endif /* PluginReportLifecycle_h */
